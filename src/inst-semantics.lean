29938dddae7a715054b6ae873593f77250417863
Benjamin Ryjikov <63170231+bryjikov@users.noreply.github.com>
2021-12-18 16:13:53 -0500

    trying inst

--- src/inst-semantics.lean
+++ src/inst-semantics.lean
@@ -11,12 +11,6 @@ structure relation := (name : name) (sigs : list sig)
 
 structure atom := (name : name) (sig : sig)
 
--- so that binds can take in sigs or relations
-inductive atom_or_sig_or_rel : Type
-| atom : atom → atom_or_sig_or_rel
-| sig : sig → atom_or_sig_or_rel
-| rel : relation → atom_or_sig_or_rel
-
 def sig_bound := sig → list atom
 
 structure sig_bounds :=
@@ -28,5 +22,22 @@ def rel_bound := relation → list (list atom)
 structure rel_bounds :=
 (lower : rel_bound)
 (upper : rel_bound)
+
+structure all_bounds :=
+(sigs : sig_bounds)
+(rels : rel_bounds)
+
+--For now, ignore join
+--For now, each bind must include a list of atoms
+inductive inst : Type
+| and : inst → inst → inst
+| sig_in_atoms : sig → list atom → inst
+| atoms_in_sig : list atom → sig → inst
+| rel_in_atoms : relation → list atom → inst
+| atoms_in_rel : list atom → relation → inst
+
+def refine_bounds : inst → all_bounds → all_bounds
+| (inst.and i1 i2) (bounds : all_bounds) := refine_bounds i2 (refine_bounds i1 bounds)
+| (inst.sig_in_atoms) (bounds : all_bounds) := ....
 
 end LoVe
