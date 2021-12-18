7785c48e3f291e59948ea73886e589ac1f508449
Benjamin Ryjikov <63170231+bryjikov@users.noreply.github.com>
2021-12-18 14:57:09 -0500

    using structure instead of inductive when we can

--- src/inst-semantics.lean
+++ src/inst-semantics.lean
@@ -5,18 +5,11 @@ set_option pp.generalized_field_notation false
 
 namespace LoVe
 
-inductive sig : Type
-| extend : name → sig → sig
-| root : sig
+structure sig := (name : name)
 
-inductive relation : Type
--- sigs are relations of arity 1
-| sig : sig → relation
--- if you want a relation of arity more than 1
-| rel : sig → relation → relation
+structure relation := (name : name) (sigs : list sig)
 
-inductive atom : Type
-| atom : name → sig → atom
+structure atom := (name : name) (sig : sig)
 
 -- so that binds can take in sigs or relations
 inductive atom_or_sig_or_rel : Type
